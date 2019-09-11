5ad9e9a4163daaac0b340621b7f3fcbe7644ff53
Rosswell Tiongco <rosswelltiongco@gmail.com>
2019-09-11 00:39:52 -0700

    Completely revamped website

---
+++ src/data.js
@@ -0,0 +1,323 @@
+module.exports = {
+    "socials": [
+        {
+            "icon": "twitter",
+            "url": "http://twitter/"
+        },
+        {
+            "icon": "github-circled",
+            "url": "https://github.com/rosswelltiongco"
+        },
+        {
+            "icon": "dribbble",
+            "url": ""
+        },
+        {
+            "icon": "skype",
+            "url": "htttp://skype"
+        },
+        {
+            "icon": "mail-alt",
+            "url": "mailto: email"
+        },
+        {
+            "icon": "linkedin-squared",
+            "url": "https://www.linkedin.com/in/rosswelltiongco/"
+        }
+    ],
+    "experiences": [
+        {
+            "__key": "69d978fd-cf0b-4afb-9fef-acef6dc366b7",
+            "title": "Software Engineer Intern at Raytheon",
+            "timeline": "May 2019 - Present",
+            "description": "blank",
+            "responsibilities": [
+                "Built tool to automate documentation",
+                "Converted old Microsoft Access Server to C# Webform to add, modify, search, and delete records"
+            ]
+        },
+        {
+            "__key": "caaac359-09ee-45c7-8683-4e8e8ec4545d",
+            "title": "Software Engineer Intern at Filtec",
+            "timeline": "May 2018 - August 2018",
+            "responsibilities": [
+                "Wrote python tool to parse and query thousands of console log entries into a sqlite database",
+                "Implemented full stack flask application to continuously monitor in house test machines",
+                "Tested and documented software builds to ensure system and customer requirements are met"
+            ],
+            "description": ""
+        },
+        {
+            "__key": "8f481f38-21ad-48f8-b690-452012707e3d",
+            "title": "Computer Science Instructional Student at CSULB",
+            "timeline": "Jan 2018 - May 2018",
+            "description": "blank",
+            "responsibilities": [
+                "Communicated with students to ensure laboratory objectives are met on time",
+                "Facilitated students’ learning of Python  laboratory projects to resolve programming conceptual issues",
+                "Evaluated assignments from 80+ students to provide fair assessment"
+            ]
+        }
+    ],
+    "name": "Rosswell Tiongco",
+    "header": "Hi, I'm Rosswell Tiongco",
+    "cta": {
+        "label": "Get my resume",
+        "url": "https://docs.google.com/document/d/1c5SpXp80180bg4zBVnay4SjwYUkzovjc-WCVCLDlbcg/edit?usp=sharing"
+    },
+    "testimonials": [
+        {
+            "__key": "a7d9842a-b7d7-48e8-bd7a-9a15ca622c69",
+            "quote": "Incredibly talented and hardworking. A super friendly guy who is a frequent communicator",
+            "name": "Amani Aryan",
+            "title": "Manager at Raytheon"
+        },
+        {
+            "__key": "f1a3454f-b08e-4511-8481-0244be593ae2",
+            "quote": "He's great person to work with",
+            "name": "Shane Peterson",
+            "title": "Quality Assurance Engineer at Filtec"
+        },
+        {
+            "__key": "e9253233-0e97-4669-b0c9-98ad8a19944c",
+            "quote": "he da bomb",
+            "name": "Frank Mendez",
+            "title": "Telecommunications"
+        },
+        {
+            "__key": "be95b73a-ac28-426c-a633-7683d8ae9c79",
+            "quote": "he put in the hours",
+            "name": "Neil Terrell",
+            "title": "Professor at CSULB"
+        }
+    ],
+    "title": "Rosswell Tiongco",
+    "projects": [
+        {
+            "__key": "284a06aa-0146-4da3-9490-bfeae404bff5",
+            "name": "CSULB Room Finder",
+            "description": "An app that helps students find open classrooms to study in",
+            "tags": [
+                "Python",
+                "Javascript",
+                "SQL"
+            ],
+            "alt": "Combustion Screenshot",
+            "img": "combustion.png",
+            "url": "https://csulb-room-finder-alpha.rosswelltiongco.repl.run"
+        },
+        {
+            "__key": "d1ba0ea9-63ca-46a9-a765-9e81b8fd4c7a",
+            "name": "CSULB Search",
+            "description": "A tool to search for csulb classes with multiple filters",
+            "tags": [
+                "Python",
+                "SQL",
+                "HTML",
+                "CSS",
+                "Javascript",
+                "jQuery"
+            ],
+            "img": "merc-01.png",
+            "url": "https://jsfiddle.net/bd2g3p4x/2/show"
+        },
+        {
+            "__key": "315b90fb-755d-4aa9-9c82-26fcb4b89e99",
+            "name": "yelpERS",
+            "description": "An app to decide where to eat for you based on your filters",
+            "tags": [
+                "Python",
+                "Javascript",
+                "SQL"
+            ],
+            "alt": "Combustion Screenshot",
+            "img": "combustion.png",
+            "url": "https://csulb-room-finder-alpha.rosswelltiongco.repl.run"
+        },
+        {
+            "__key": "5432757c-7888-4d39-9a12-3ceab8f8b31c",
+            "name": "Rate My Professor CSULB Chrome App",
+            "description": "An app that helps students find open classrooms to study in",
+            "tags": [
+                "Python",
+                "Javascript",
+                "SQL"
+            ],
+            "alt": "Combustion Screenshot",
+            "img": "combustion.png",
+            "url": "https://csulb-room-finder-alpha.rosswelltiongco.repl.run"
+        },
+        {
+            "__key": "cc49d30c-afa6-481d-bfad-092acd6c64f8",
+            "name": "CSULB Rate My Professor Bookmarklet",
+            "description": "An app that helps students find open classrooms to study in",
+            "tags": [
+                "Python",
+                "Javascript",
+                "SQL"
+            ],
+            "alt": "Combustion Screenshot",
+            "img": "combustion.png",
+            "url": "https://csulb-room-finder-alpha.rosswelltiongco.repl.run"
+        },
+        {
+            "__key": "eca401e6-0e2a-4419-99ab-44139711afa4",
+            "name": "Beer Pong Robot",
+            "description": "An app that helps students find open classrooms to study in",
+            "tags": [
+                "Python",
+                "Javascript",
+                "SQL"
+            ],
+            "alt": "Combustion Screenshot",
+            "img": "combustion.png",
+            "url": "https://csulb-room-finder-alpha.rosswelltiongco.repl.run"
+        },
+        {
+            "__key": "c4193614-cbd9-4e13-b517-a3e34d2dbf9e",
+            "name": "Obstacle avoiding robot",
+            "description": "An app that helps students find open classrooms to study in",
+            "tags": [
+                "Python",
+                "Javascript",
+                "SQL"
+            ],
+            "alt": "Combustion Screenshot",
+            "img": "combustion.png",
+            "url": "https://csulb-room-finder-alpha.rosswelltiongco.repl.run"
+        },
+        {
+            "__key": "41356164-c1a9-45ab-92de-c7785bb710d7",
+            "name": "Smart Car and Home",
+            "description": "An app that helps students find open classrooms to study in",
+            "tags": [
+                "Python",
+                "Javascript",
+                "SQL"
+            ],
+            "alt": "Combustion Screenshot",
+            "img": "combustion.png",
+            "url": "https://csulb-room-finder-alpha.rosswelltiongco.repl.run"
+        },
+        {
+            "__key": "b114801e-e8aa-4aac-9a01-86b415eea9b6",
+            "name": "UART",
+            "description": "An app that helps students find open classrooms to study in",
+            "tags": [
+                "Python",
+                "Javascript",
+                "SQL"
+            ],
+            "alt": "Combustion Screenshot",
+            "img": "combustion.png",
+            "url": "https://csulb-room-finder-alpha.rosswelltiongco.repl.run"
+        },
+        {
+            "__key": "a699b1fd-a716-4df2-a109-fa9433ca285f",
+            "name": "Micromouse",
+            "description": "An app that helps students find open classrooms to study in",
+            "tags": [
+                "Python",
+                "Javascript",
+                "SQL"
+            ],
+            "alt": "Combustion Screenshot",
+            "img": "combustion.png",
+            "url": "https://csulb-room-finder-alpha.rosswelltiongco.repl.run"
+        },
+        {
+            "__key": "2dc8a443-b431-489b-b634-a51dd7e82ec9",
+            "name": "Robotic Arm",
+            "description": "An app that helps students find open classrooms to study in",
+            "tags": [
+                "Python",
+                "Javascript",
+                "SQL"
+            ],
+            "alt": "Combustion Screenshot",
+            "img": "combustion.png",
+            "url": "https://csulb-room-finder-alpha.rosswelltiongco.repl.run"
+        },
+        {
+            "__key": "f5d15fe6-36be-4af9-a5a5-40a1099fe540",
+            "name": "FM Transmitter",
+            "description": "An app that helps students find open classrooms to study in",
+            "tags": [
+                "Python",
+                "Javascript",
+                "SQL"
+            ],
+            "alt": "Combustion Screenshot",
+            "img": "combustion.png",
+            "url": "https://csulb-room-finder-alpha.rosswelltiongco.repl.run"
+        },
+        {
+            "__key": "04caf403-1df4-4c41-b59b-47048eab10b1",
+            "name": "Home Security System",
+            "description": "An app that helps students find open classrooms to study in",
+            "tags": [
+                "Python",
+                "Javascript",
+                "SQL"
+            ],
+            "alt": "Combustion Screenshot",
+            "img": "combustion.png",
+            "url": "https://csulb-room-finder-alpha.rosswelltiongco.repl.run"
+        },
+        {
+            "__key": "9ebdf7ea-fcaa-4aeb-9337-fcf309936fee",
+            "name": "MIPS Processor",
+            "description": "An app that helps students find open classrooms to study in",
+            "tags": [
+                "Python",
+                "Javascript",
+                "SQL"
+            ],
+            "alt": "Combustion Screenshot",
+            "img": "combustion.png",
+            "url": "https://csulb-room-finder-alpha.rosswelltiongco.repl.run"
+        },
+        {
+            "__key": "c17611f4-65fb-4675-bfd7-c84a7e7a2bf6",
+            "name": "IoT Smart Trash",
+            "description": "An app that helps students find open classrooms to study in",
+            "tags": [
+                "Python",
+                "Javascript",
+                "SQL"
+            ],
+            "alt": "Combustion Screenshot",
+            "img": "combustion.png",
+            "url": "https://csulb-room-finder-alpha.rosswelltiongco.repl.run"
+        },
+        {
+            "__key": "ca4b9db5-57f0-45cb-a07d-265d6120c216",
+            "name": "Pong Game",
+            "description": "An app that helps students find open classrooms to study in",
+            "tags": [
+                "Python",
+                "Javascript",
+                "SQL"
+            ],
+            "alt": "Combustion Screenshot",
+            "img": "combustion.png",
+            "url": "https://csulb-room-finder-alpha.rosswelltiongco.repl.run"
+        },
+        {
+            "__key": "85857f5c-ac3f-42db-b630-8b80ac022f01",
+            "name": "Presidential Tweets",
+            "description": "An app that helps students find open classrooms to study in",
+            "tags": [
+                "Python",
+                "Javascript",
+                "SQL"
+            ],
+            "alt": "Combustion Screenshot",
+            "img": "combustion.png",
+            "url": "https://csulb-room-finder-alpha.rosswelltiongco.repl.run"
+        }
+    ],
+    "email": "rosswelltiongco@gmail.com",
+    "description": "I'm a rising college senior currently taking up Computer Engineering California State University, Long Beach. I'm an engineer with 3 years of professional experience, having worked with clients such as Raytheon and Filtec. I enjoy building everything from practical applications to goofy applications.",
+    "footerTitle": "Software Engineer"
+}
